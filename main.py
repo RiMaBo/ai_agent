ff334548e495cfd428fa5ea4863ca2a440871295
RiMaBo <rimabo@gmail.com>
2025-07-08 15:35:34 -0600

    LLMs: Input

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os
+import sys
+
 from dotenv import load_dotenv
 from google import genai
 
@@ -6,9 +8,15 @@ from google import genai
 def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
-
     client = genai.Client(api_key=api_key)
-    response = client.models.generate_content(model="gemini-2.0-flash-001", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+
+    args = sys.argv
+    if len(args) < 2:
+        print(f"Usage: python {sys.argv[0]} <input prompt>")
+        sys.exit(1)
+
+    prompt = sys.argv[1]
+    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=prompt)
 
     print(response.text)
     print("Prompt tokens: " + str(response.usage_metadata.prompt_token_count))

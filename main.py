dbeb823d1e3692d7f51f95cc3f33418eb3e9618a
RiMaBo <rimabo@gmail.com>
2025-07-08 16:04:01 -0600

    LLMs: Verbose

--- main.py
+++ main.py
@@ -11,21 +11,27 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
-    args = sys.argv
-    if len(args) < 2:
-        print(f"Usage: python {sys.argv[0]} <input prompt>")
+    verbose = "--verbose" in sys.argv
+    args = [arg for arg in sys.argv[1:] if not arg.startswith("--")]
+    app_name = sys.argv[0]
+
+    if not args:
+        print(f"Usage: python {app_name} <input prompt> [--verbose]")
         sys.exit(1)
 
-    user_prompt = sys.argv[1]
+    user_prompt = " ".join(args)
     messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
     response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
 
+    if verbose:
+        print(f"User prompt: {user_prompt}\n")
+        print("Prompt tokens: " + str(response.usage_metadata.prompt_token_count))
+        print("Response tokens: " + str(response.usage_metadata.candidates_token_count))
+
     print("Response:")
     print(response.text)
-    print("Prompt tokens: " + str(response.usage_metadata.prompt_token_count))
-    print("Response tokens: " + str(response.usage_metadata.candidates_token_count))
 
 
 if __name__ == "__main__":

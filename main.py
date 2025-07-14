18d96d4616f9bd7113a5d7df9eee18b5e12eb52b
RiMaBo <rimabo@gmail.com>
2025-07-14 13:48:05 -0600

    Function Calling: System Prompt

--- main.py
+++ main.py
@@ -5,11 +5,14 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
+from prompts import system_prompt
+
 
 def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
+    model_name = "gemini-2.0-flash-001"
 
     verbose = "--verbose" in sys.argv
     args = [arg for arg in sys.argv[1:] if not arg.startswith("--")]
@@ -23,7 +26,11 @@ def main():
     messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
-    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
+    response = client.models.generate_content(
+        model=model_name,
+        contents=messages,
+        config=types.GenerateContentConfig(system_instruction=system_prompt),
+    )
 
     if verbose:
         print(f"User prompt: {user_prompt}\n")

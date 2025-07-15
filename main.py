dd3937560c54c33b228c5ca15d3bff66d6ddb6cf
RiMaBo <rimabo@gmail.com>
2025-07-15 16:49:58 -0600

    Function Calling: Function Declaration

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from google import genai
 from google.genai import types
 
 from prompts import system_prompt
+from call_function import available_functions
 
 
 def main():
@@ -29,7 +30,7 @@ def main():
     response = client.models.generate_content(
         model=model_name,
         contents=messages,
-        config=types.GenerateContentConfig(system_instruction=system_prompt),
+        config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt),
     )
 
     if verbose:
@@ -37,8 +38,13 @@ def main():
         print("Prompt tokens: " + str(response.usage_metadata.prompt_token_count))
         print("Response tokens: " + str(response.usage_metadata.candidates_token_count))
 
-    print("Response:")
-    print(response.text)
+    if not response.function_calls:
+        print("Response:")
+        print(response.text)
+
+    for function_call_part in response.function_calls:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+
 
 
 if __name__ == "__main__":
